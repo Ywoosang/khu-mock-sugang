818faa3cfe4cc96dadb0e73e28d2743d30d9cc1a
Ywoosang <opellong13@gmail.com>
2021-02-17 03:59:53 +0900

    Add notice menu

---
+++ src/components/NoticeSection.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="content-panel">
+  <notice-content></notice-content>
+  <notice-modal></notice-modal>
+</div>
+</template>
+
+<script>
+import NoticeModal from "./NoticeModal.vue"
+import NoticeContent from "./NoticeContent.vue"
+export default {
+    components : {
+        'notice-modal' : NoticeModal,
+        'notice-content' : NoticeContent
+    }
+
+}
+</script>
+
+<style>
+.content-panel {
+    padding: 25px;
+    padding-top: 15px;
+    box-sizing: border-box;
+    margin-top:85px;
+    width: calc(100vw - 120px);
+}
+.select{
+  overflow: hidden;
+}
+</style>
