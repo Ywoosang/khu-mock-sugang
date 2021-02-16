a664c1071f269997f08e257c8410dd372bfe0d9f
Ywoosang <opellong13@gmail.com>
2021-02-17 03:55:19 +0900

    Implement menu change functionality

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -7,7 +7,7 @@
       </div>
       <div class="wrap-menu">
         <ul>
-          <li class="m-el notice">
+          <li @click="toNotice" v-bind:class="{'selected-menu':menu.isNotice}" class="m-el notice">
             <img src="icon-notice.png" />
             <h4 class="menu-kor">공지사항</h4>
           </li>
@@ -15,15 +15,15 @@
             <img src="icon-planning.png" />
             <h4 class="menu-kor">종합시간표 조회</h4>
           </li>
-          <li class="m-el">
+          <li @click="toWishList" class="m-el">
             <img src="icon-subject.png" />
             <h4 class="menu-kor">희망과목내역</h4>
           </li>
-          <li class="m-el">
+          <li @click="toCourseList" v-bind:class="{'selected-menu':menu.isCourseList}" class="m-el">
             <img src="icon-search.png" />
             <h4 class="menu-kor">수강신청내역</h4>
           </li>
-          <li class="m-el registration selected-menu">
+          <li @click="toRegister" class="m-el registration" v-bind:class="{'selected-menu':menu.isRegister}">
             <img src="icon-registration.png" />
             <h4 class="menu-kor">수강신청</h4>
           </li>
@@ -35,6 +35,42 @@
 
 <script>
 export default {
+  data(){
+    return {
+      menu :{
+      isCourseList:false,
+      isRegister:false,
+      isNotice:true,
+      }
+    }
+  },
+  methods: {
+    // 공지사항탭
+    toNotice(){
+       for(let menu in this.menu){
+        menu === 'isNotice' ? this.menu[menu] = true : this.menu[menu] =false;
+      }
+      this.$emit('noticeMenu')
+    }, 
+    //희망과목탭
+    toWishList(){
+      this.$emit('wishMenu')
+    },
+    //신청내역탭
+    toCourseList(){
+      for(let menu in this.menu){
+        menu === 'isCourseList' ? this.menu[menu] = true : this.menu[menu] =false;
+      }
+      this.$emit('courseMenu')
+    },
+    //수강신청탭
+    toRegister(){
+      for(let menu in this.menu){
+        menu === 'isRegister' ? this.menu[menu] = true : this.menu[menu] =false;
+      }
+      this.$emit('registerMenu')
+    }
+  }
 
 };
 </script>
