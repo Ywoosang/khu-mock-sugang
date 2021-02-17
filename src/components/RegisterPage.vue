fda3e610705036058983fdfe8c095de09f17a4f9
Ywoosang <opellong13@gmail.com>
2021-02-17 22:21:14 +0900

    Refactor RegisterPage.vue

--- src/components/RegisterPage.vue
+++ src/components/RegisterPage.vue
@@ -8,37 +8,45 @@
       v-bind:courseDelete="isDelete"
     ></modal-window>
     <side-bar
-    @noticeMenu="toNoticeMenu"
-    @courseMenu="toCourseMenu"
-    @registerMenu="toRegisterMenu"
+      @noticeMenu="toNoticeMenu"
+      @courseMenu="toCourseMenu"
+      @registerMenu="toRegisterMenu"
+      @wishMenu="toWishMenu"
     ></side-bar>
     <section class="main">
-      <app-header ></app-header>
-      <notice-menu v-if="noticeMenu"></notice-menu>
-      <div v-if="contentTotal" class="content">
-        <select-tab
-          v-bind:defaultTab="defaultValue"
-          @tab="changeTab"
-        ></select-tab>
-        <template v-if="defaultValue">
-          <wish-title></wish-title>
-          <wish-content
-            @apply="registerCourse"
-            @save="contentSave"
-            v-bind:savedCourses="savedCourses"
-            v-bind:courseFull="courseFull"
-          ></wish-content>
-        </template>
-        <template v-if="!defaultValue">
-          <search-title></search-title>
-          <search-content
-            @apply="registerCourse"
-            v-bind:modalPause="isModal"
-            v-bind:isSearch="isSearch"
-          ></search-content>
-        </template>
-      </div>
-      <course-list v-if="listTotal" @delete="deleteCourse" v-bind:courseList="courseList"></course-list>
+      <app-header @home="goHomePage" @timeOver="goTimeOutHomePage"></app-header>
+      <notice-menu @home="goHomePage" v-if="noticeMenu"></notice-menu>
+      <wish-menu v-if="wishMenu"></wish-menu>
+      <time-limit v-if="timeLimit>0 && registerMenu" v-bind:timeLimit="timeLimit"></time-limit>
+        <div v-if="contentTotal && timeLimit<=0" class="content">
+          <select-tab
+            v-bind:defaultTab="defaultValue"
+            @tab="changeTab"
+          ></select-tab>
+          <template v-if="defaultValue">
+            <wish-title></wish-title>
+            <wish-content
+              @apply="registerCourse"
+              @save="contentSave"
+              v-bind:savedCourses="savedCourses"
+              v-bind:courseFull="courseFull"
+            ></wish-content>
+          </template>
+          <template v-if="!defaultValue">
+            <search-title v-bind:searchNum="searchNum"></search-title>
+            <search-content
+              @apply="registerCourse"
+              @searchNum="searchNumber"
+              v-bind:modalPause="isModal"
+              v-bind:isSearch="isSearch"
+            ></search-content>
+          </template>
+        </div>
+        <course-list v-if="(timeLimit<=0 || courseMenu) && listTotal"
+          @delete="deleteCourse"
+          v-bind:courseList="courseList"
+          v-bind:courseMenu="courseMenu"
+        ></course-list>
     </section>
     <app-footer></app-footer>
   </div>
@@ -56,47 +64,77 @@ import CourseList from "./CourseList.vue";
 import AppFooter from "./AppFooter.vue";
 import Modal from "./ModalWindow.vue";
 import NoticeMenu from "./NoticeSection.vue";
+import WishMenu from "./WishSection.vue";
+import TimeLimit from "./TimeLimit.vue";
 
 export default {
+  props :['timeLimit'],
   data() {
     return {
       defaultValue: true,
       isSearch: false,
-      isDelete:false,
-      isModal:false,
+      isDelete: false,
+      isModal: false,
       modalMessage: null,
       courseList: [],
-      savedCourses:null,
-      pause:false,
+      savedCourses: null,
+      pause: false,
+      //
+      searchNum: 10,
       //탭 변경
-      contentTotal:false,
-      listTotal:false, 
-      noticeMenu:true,
+      contentTotal: false,
+      listTotal: false,
+      noticeMenu: true,
+      wishMenu: false,
       // 수강신청 내역일 경우
+      courseMenu: false,
+      registerMenu : false,
+      // 시간초과시 메인으로
+      timeOut: false,
+      //
     };
   },
-  computed:{
-    courseFull(){
-      // if(this.courseList.length >= 7) return true;
-      return false; 
-    } 
+  computed: {
+    courseFull() {
+      if(this.courseList.length >= 7) return true;
+      return false;
+    },
+
   },
   methods: {
     // 메뉴 변경
-    toNoticeMenu(){
-        this.noticeMenu=true;
-        this.listTotal=false;
-        this.contentTotal=false;
+    toNoticeMenu() {
+      this.registerMenu = false;
+      this.wishMenu = false;
+      this.noticeMenu = true;
+      this.listTotal = false;
+      this.contentTotal = false;
     },
-    toCourseMenu(){
-        this.noticeMenu=false;
-        this.contentTotal = false;
-        this.listTotal = true; 
+    toWishMenu() {
+      this.registerMenu = false;
+      this.wishMenu = true;
+      this.noticeMenu = false;
+      this.listTotal = false;
+      this.contentTotal = false;
+      //
     },
-    toRegisterMenu(){
-        this.noticeMenu=false;
-        this.contentTotal = true;
-        this.listTotal = true; 
+    toCourseMenu() {
+      this.registerMenu = false;
+      this.wishMenu = false;
+      this.noticeMenu = false;
+      this.contentTotal = false;
+      this.listTotal = true;
+      //
+      this.courseMenu = true;
+    },
+    toRegisterMenu() {
+      this.registerMenu = true;
+      this.wishMenu = false;
+      this.noticeMenu = false;
+      this.contentTotal = true;
+      this.listTotal = true;
+      //
+      this.courseMenu = false;
     },
     // 수강신청 페이지
     changeTab() {
@@ -105,54 +143,68 @@ export default {
     searchCourse(isSearch) {
       this.isSearch = isSearch;
     },
-    deleteCourse(num){
-        // 삭제문구 전송
-      if(this.pause) return;
-      this.pause =true;
+    deleteCourse(num) {
+      // 삭제문구 전송
+      if (this.pause) return;
+      this.pause = true;
       this.isDelete = true;
-      this.showModal(`[삭제연습${num}] 을 삭제 하시겠습니까?`)
+      this.showModal(`[삭제연습${num}] 을 삭제 하시겠습니까?`);
     },
     showModal(msg) {
-    // 모달창 생성 
+      // 모달창 생성
       this.isModal = true;
       this.modalMessage = msg;
     },
     registerCourse(num) {
-      this.isDelete =false; 
-      if(this.pause) return;
-      this.pause =true;
+      this.isDelete = false;
+      if (this.pause) return;
+      this.pause = true;
       if (num === 10) {
-          this.showModal("수강 인원이 초과되었습니다.");
+        this.showModal("수강 인원이 초과되었습니다.");
         return;
       }
       if (this.courseList.length >= 7) {
-          this.showModal("이수가능학점을 초과했습니다.");
+        this.showModal("이수가능학점을 초과했습니다.");
         return;
       }
-        this.showModal("신청되었습니다.");
-        this.courseList.push(this.courseList.length + 1);
+      this.showModal("신청되었습니다.");
+      this.courseList.push(this.courseList.length + 1);
     },
     modalClose() {
+      if (this.timeOut) this.$emit("timeOver");
       this.isModal = false;
-      this.pause =false; 
+      this.pause = false;
     },
     // 삭제확인일 경우
-    modalAccept(){
-      if(this.isDelete) {
-        this.isDelete=false;
+    modalAccept() {
+      if (this.timeOut) this.$emit("timeOver");
+      if (this.isDelete) {
+        this.isDelete = false;
         this.isModal = false;
-        setTimeout(()=>{
-            this.isModal=true; 
-            this.showModal('삭제되었습니다.');
-            this.courseList.pop();
-        },1700);
+        setTimeout(() => {
+          this.isModal = true;
+          this.showModal("삭제되었습니다.");
+          this.courseList.pop();
+        }, 1700);
       }
       this.isModal = false;
-      this.pause =false; 
+      this.pause = false;
     },
-    contentSave(contents){
-      this.savedCourses = contents; 
+    contentSave(contents) {
+      this.savedCourses = contents;
+    },
+    searchNumber(num) {
+      this.searchNum = num;
+    },
+    goTimeOutHomePage() {
+      this.showModal("장시간 사용이 없어서 자동로그아웃 됩니다!");
+      this.timeOut = true;
+    },
+    goHomePage() {
+      this.$emit("home"); 
     }
+    
+    
   },
   components: {
     "app-header": AppHeader,
@@ -166,6 +218,8 @@ export default {
     "app-footer": AppFooter,
     "modal-window": Modal,
     "notice-menu": NoticeMenu,
+    "wish-menu": WishMenu,
+    "time-limit": TimeLimit
   },
 };
 </script>
@@ -173,15 +227,21 @@ export default {
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500&display=swap");
 html,
-body {
+body,
+.totalWrapper {
   height: 100%;
   width: 100%;
   margin: 0;
   font-family: "Noto Sans KR", sans-serif;
   font-weight: 300;
 }
-body {
+body,
+.totalWrapper {
   position: relative;
+}
+.totalWrapper {
+  display: block;
+  height: 100vh;
 }
 div {
   box-sizing: border-box;
