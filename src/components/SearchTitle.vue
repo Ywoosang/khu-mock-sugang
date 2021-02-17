fe50482d07d051550b36d95fc9c20d3ce1bacd7b
Ywoosang <opellong13@gmail.com>
2021-02-17 22:23:05 +0900

    Add add props data to show number of searched course

--- src/components/SearchTitle.vue
+++ src/components/SearchTitle.vue
@@ -2,14 +2,16 @@
   <div class="sw-header search-header">
     <h2>개설강좌 목록</h2>
     <div class="buttonset">
-      <span class="item">총 조회건수<em class="totalSearchNumber">10</em>건</span
+      <span class="item">총 조회건수<em class="totalSearchNumber">{{ searchNum }}</em>건</span
       >
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props:['searchNum']
+};
 </script>
 
 <style>
