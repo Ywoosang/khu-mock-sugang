788abbdb9cce733978f3a09af21ec67d5d107e95
Ywoosang <opellong13@gmail.com>
2021-02-19 09:09:34 +0900

    Move notice menu to shared folder

---
+++ src/components/menu/NoticeMenu.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="content-wrapper">
+    <notice-modal v-if="modal" 
+    @hideModal="hideModal"
+    @home="$emit('home')"></notice-modal>
+    <notice-content></notice-content>
+  </div>
+</template>
+
+<script>
+import NoticeModal from "../shared/NoticeModal.vue"; 
+import NoticeContent from  "../shared/NoticeContent.vue";
+
+export default {
+  data(){
+      return {
+          modal:true
+      }
+  }, 
+  created(){
+      this.modal = true;
+  },
+  components: {
+    NoticeModal,
+    NoticeContent,
+  },
+  methods : {
+      hideModal(){
+          this.modal = false;
+      }
+  }
+};
+</script>
+
+<style>
+</style>
