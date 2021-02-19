688d68941675956d38afe3ecc893cf1bbdf3f403
Ywoosang <opellong13@gmail.com>
2021-02-19 09:12:13 +0900

    Move title components to shared folder

---
+++ src/components/shared/SearchTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sw-header search-header">
+    <h2>개설강좌 목록</h2>
+    <div class="buttonset">
+      <span class="item">총 조회건수<em class="totalSearchNumber">{{ searchNum }}</em>건</span
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['searchNum']
+};
+</script>
+
+<style>
+</style>
