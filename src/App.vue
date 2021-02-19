28b39d387b97debf3b05617bbcc4200da1347fb5
Ywoosang <opellong13@gmail.com>
2021-02-19 09:05:20 +0900

    Use header components

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,86 @@
 <template>
-<span :class="{'totalWrapper':!home}">
-  <register-page v-bind:timeLimit="timeLimit" @timeOver="renderHome" @home="renderHome" v-if="!home"></register-page>
-  <home-page @register="startTest"  v-else></home-page>
-</span>
+  <div class="main">
+    <modal
+      v-if="isModal"
+      v-bind:modalMessage="modalMessage"
+      @accept="renderHome"
+      @close="renderHome"
+    ></modal>
+    <template v-if="home">
+      <home-header @home="renderHome"></home-header>
+      <home-page @register="startTest" @home="renderHome"></home-page>
+    </template>
+    <template v-else>
+      <course-header @home="renderHome" @timeOver="changeTimeOver"></course-header>
+      <course-page
+        @home="renderHome"
+        v-bind:timeLimit="timeLimit"
+        v-bind:menu="menu"
+      ></course-page>
+    </template>
+    <side-bar v-bind:homePage="home" @changeMenu="changeMenu"></side-bar>
+  </div>
 </template>
-
+ 
 <script>
-import RegisterPage from "./components/RegisterPage.vue";
+import CoursePage from "./components/CoursePage.vue";
 import HomePage from "./components/HomePage.vue";
+import SideBar from "./components/common/SideBar.vue";
+import Modal from "./components/common/Modal.vue";
+import HomeHeader from "./components/common/HomeHeader.vue";
+import CourseHeader from "./components/common/CourseHeader.vue";
 export default {
-  data(){
+  data() {
     return {
-      timeLimit : 30,
-      home : true,
-    }
+      // 헤더에서 시간종료
+      //수강신청 시작시 기다리는 시간
+      timeLimit: 0,
+      // 홈페이지 여부
+      home: true,
+      // 기본적으로 공지사항 탭으로 이동
+      menu: "noticeMenu",
+      // 시간 초과 모달
+      isModal: false,
+      modalMessage: "장시간 사용이 없어서 자동로그아웃 됩니다!",
+    };
   },
-  methods :{
-    renderHome(){
-      this.timeLimit =0; 
-      this.home= true; 
+  methods: {
+    // 체인지 이벤트 받은 후 인자를 데이터에 바인딩해서 전달
+    changeMenu(menu) {
+      if (this.home == true) return;
+      this.menu = menu;
     },
-    startTest(){
-      const timmer = setInterval(()=>{
-        this.timeLimit-- 
-      },1000)
-      if(this.timeLimit==0){
+    renderHome() {
+      this.isModal = false;
+      this.timeOver = false;
+      this.timeLimit = 0;
+      this.home = true;
+      location.reload();
+    },
+    startTest() {
+      const timmer = setInterval(() => {
+        this.timeLimit--;
+      }, 1000);
+      if (this.timeLimit == 0) {
         clearInterval(timmer);
         // 사전 오류 방지
-        this.timeLimit=0; 
+        this.timeLimit = 0;
       }
-      this.home=false;
-    }
+      this.home = false;
+    },
+    changeTimeOver() {
+      this.isModal = true;
+    },
   },
-  components:{
-    "register-page": RegisterPage,
-    "home-page" : HomePage,
+  components: {
+    "course-page": CoursePage,
+    "home-page": HomePage,
+    SideBar,
+    Modal,
+    HomeHeader,
+    CourseHeader,
   },
-}
+};
 </script>
 
 <style>
