7a0796adccfef8422894ba80315215f1b3420887
Ywoosang <opellong13@gmail.com>
2021-02-19 09:07:47 +0900

    Add home header

---
+++ src/components/common/HomeHeader.vue
@@ -0,0 +1,14 @@
+<template>
+ <header  class='header-login'>
+       <div class="control is-opened" title="메뉴닫기"></div>
+        <div title="경희대학교 수강신청시스템">
+            <img src="logo2.png">
+        </div>
+ </header>
+</template>
+<script>
+ 
+</script>
+
+<style>
+</style>
