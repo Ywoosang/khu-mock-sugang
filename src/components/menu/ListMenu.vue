e48ddf12503d9cac11c164ac8726ccd2dfe63469
Ywoosang <opellong13@gmail.com>
2021-02-19 09:08:34 +0900

    Add list menu

---
+++ src/components/menu/ListMenu.vue
@@ -0,0 +1,19 @@
+<template>
+   <div class="content-wrapper">
+       <course-list v-bind:courseList="courseList" v-bind:listMenu="true"></course-list>
+   </div>
+</template>
+
+<script>
+import CourseList from '../shared/CourseList.vue'
+export default {
+  props : ['courseList'],
+  components: { 
+      CourseList 
+    },
+}
+</script>
+
+<style>
+
+</style>
